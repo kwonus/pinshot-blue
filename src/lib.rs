dee789c135fab3c2f7e040cda6d43f2016e6151c
Kevin Wonus <kevin@wonus.com>
2023-03-25 00:27:02 -0400

    Refined the Quell-AVX syntax

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,58 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use std::any::Any;
-use std::borrow::Borrow;
 use pest::Parser;
 use pest::iterators::Pairs;
+use serde::Serialize;
+use serde_json::Result;
 
 #[derive(Parser)]
 #[grammar = "avx-quelle.pest"]
 struct QuelleParser;
 
 struct ParseResult {
+    parse: Vec<Parsed>,
+    error: String,
+}
+
+#[derive(Serialize)]
+struct Parsed {
+    rule: String,
+    text: String,
+    children: Vec<Parsed>,
+}
+
+#[derive(Serialize)]
+struct RootParse {
+    input: String,
+    result: Vec<Parsed>,
+    error: String,
+}
+
+#[derive(Serialize)]
+struct RawParseResult {
+    input: String,
     result: String,
     error: String,
 }
 
 fn get_parse(stmt: &str) -> ParseResult {
     let input_string = stmt.to_string();
-
+    let mut top: Vec<Parsed> = vec![];
     let task = QuelleParser::parse(Rule::statement, &input_string);
 
     if task.is_ok() {
         let pairs = task.unwrap();
-        //recurse(pairs, &mut top);
+        recurse(pairs, &mut top);
         let root = ParseResult {
-            result: pairs.to_string(),
+            parse: top,
             error: "".to_string(),
         };
         root
     }
     else if task.is_err() {
         let mut root = ParseResult {
-            result: "".to_string(),
+            parse: top,
             error: task.unwrap_err().to_string(),
         };
         if root.error.is_empty() {
@@ -42,8 +63,44 @@ fn get_parse(stmt: &str) -> ParseResult {
     }
     else {
         let root = ParseResult {
-            result: "".to_string(),
+            parse: top,
             error: "Internal Error".to_string(),
+        };
+        root
+    }
+}
+
+fn get_parse_raw(stmt: &str) -> RawParseResult {
+    let input_string = stmt.to_string();
+    let return_input_string = input_string.clone();
+
+    let task = QuelleParser::parse(Rule::statement, &input_string);
+
+    if task.is_ok() {
+        let pairs = task.unwrap();
+        let root = RawParseResult {
+            input: return_input_string,
+            error: "".to_string(),
+            result: pairs.to_string(),
+        };
+        root
+    }
+    else if task.is_err() {
+        let mut root = RawParseResult {
+            input: return_input_string,
+            error: task.unwrap_err().to_string(),
+            result: "".to_string(),
+        };
+        if root.error.is_empty() {
+            root.error = "Unknown Error".to_string();
+        }
+        root
+    }
+    else {
+        let root = RawParseResult {
+            input: return_input_string,
+            error: "Internal Error".to_string(),
+            result: "".to_string(),
         };
         root
     }
@@ -75,15 +132,53 @@ pub unsafe extern "C" fn create_quelle_parse(c_stmt: *const c_char) -> *mut c_ch
         Err(_e) => "",
     } };
 
-    let mut root = get_parse(&statement_str);
+    let mut result = get_parse(&statement_str);
 
     if statement_str.is_empty() {
-        root.error = "Invalid Input Error".to_string();
+        result.error = "Invalid Input Error".to_string();
     }
-    if root.result.is_empty() && !root.error.is_empty() {
-        root.error = "Unexpected empty result".to_string();
+    if result.parse.is_empty() && !result.error.is_empty() {
+        result.error = "Unexpected empty parse result".to_string();
     }
-    let output = if root.error.is_empty() { root.result } else { root.error };  // non-error will always begin / end with { / }
+
+    let root = RootParse {
+        input: statement_str.to_string(),
+        result: result.parse,
+        error: result.error,
+    };
+    let output: String = match serde_json::to_string(&root) {
+        Ok(r) => r,
+        Err(_err) => "{ \"error\": \"bad program design\"}".to_string(),
+    };
+
+    let output_cstr = CString::new(output).unwrap();
+    output_cstr.into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn create_quelle_parse_raw(c_stmt: *const c_char) -> *mut c_char {
+
+    // SAFETY: The caller has already guaranteed this is okay (see the
+    // `# Safety` section of the doc-comment).
+    let statement_str: &str = if c_stmt.is_null() {""} else { match std::ffi::CStr::from_ptr(c_stmt).to_str() {
+        Ok(stmt) => stmt,
+        Err(_e) => "",
+    } };
+
+    let mut raw = get_parse_raw(&statement_str);
+
+    if statement_str.is_empty() {
+        raw.error = "Invalid Input Error".to_string();
+    }
+    if raw.result.is_empty() && !raw.error.is_empty() {
+        raw.error = "Unexpected empty parse result".to_string();
+    }
+
+    let output: String = match serde_json::to_string(&raw) {
+        Ok(r) => r,
+        Err(_err) => "{ \"error\": \"bad program design\"}".to_string(),
+    };
+
     let output_cstr = CString::new(output).unwrap();
     output_cstr.into_raw()
 }
@@ -97,4 +192,26 @@ pub extern "C" fn delete_quelle_parse(c_lent: *mut c_char) -> bool {
         let _reclaim = CString::from_raw(c_lent);
         return true;
     };
+}
+
+fn recurse(children: Pairs<Rule>, items: &mut Vec<Parsed>)
+{
+    for pair in children {
+        let mut result: Vec<Parsed> = vec![];
+        let text = pair.as_str().to_string();
+        let mut rule = pair.to_string();
+        // A non-terminal pair can be converted to an iterator of the tokens which make it up:
+        recurse(pair.into_inner(), &mut result);
+
+        let paren = rule.find('(').unwrap();
+        if paren > 0 {
+            rule = rule[0..paren].to_string();
+        }
+        let item = Parsed {
+            rule: rule,
+            text: text,
+            children: result,
+        };
+        items.push(item);
+    }
 }
