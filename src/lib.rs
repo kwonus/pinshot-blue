a9d43d7fda6182e72687cae5b05911bdabc96ff5
Kevin Wonus <kevin@wonus.com>
2024-02-13 20:32:04 -0500

    Rev to 2.4.212

--- src/lib.rs
+++ src/lib.rs
@@ -242,7 +242,7 @@ pub unsafe extern "C" fn assert_grammar_revision(ymdd: u16) -> u16 {
 
 #[no_mangle]
 pub unsafe extern "C" fn get_library_revision() -> u16 {
-    return 0x4211;
+    return 0x4212;
 }
 
 fn recurse(children: Pairs<Rule>, items: &mut Vec<Parsed>)
