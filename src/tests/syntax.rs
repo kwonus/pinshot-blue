39c64e7a9ca1d2d37bbbb02a3afa8dfdc3e25788
Kevin Wonus <kevin@wonus.com>
2024-04-06 17:59:08 -0400

    4.4.7 grammar nor generates parses; fixes bugs found in 4.4.4

--- src/tests/syntax.rs
+++ src/tests/syntax.rs
@@ -1,8 +1,8 @@
-use crate::{assert_grammar_revision_internal, get_parse_raw};
+use crate::{get_parse_raw};
 
 #[test]
 fn invoke() {
-    let statement_str = "$foo-bar";
+    let statement_str = "#foo-bar";
     let raw = get_parse_raw(&statement_str);
     assert_eq!(raw.result.is_empty() && !raw.error.is_empty(), false);
 }
@@ -21,33 +21,25 @@ fn exit() {
 
 #[test]
 fn ordered_statement() {
-    let statement_str = "\"/BoV/&in|out&/prep/ /det/ begin* God \\create\\\"";
+    let statement_str = "\"/BoV/&in|out&/prep/ /det/ begin* God [create]\"";
     let raw = get_parse_raw(&statement_str);
     assert_eq!(raw.result.is_empty() && !raw.error.is_empty(), false);
 }
 #[test]
-fn unordered_compound_statement() {
-    let statement_str = "/BoV/&in|out&/prep/ + /det/ begin* ; God \\create\\";
+fn unordered_statement() {
+    let statement_str = "/BoV/&in|out&/prep/ /det/ begin* God [create]";
     let raw = get_parse_raw(&statement_str);
     assert_eq!(raw.result.is_empty() && !raw.error.is_empty(), false);
 }
 #[test]
-fn assert_correct_version() {
-    let v :u16 = 0x4303;
-    let result = assert_grammar_revision_internal(v);
-    assert_eq!(result, v);
-}
-#[test]
-fn assert_incorrect_version() {
-    let v :u16 = 0x3B02;
-    let result = assert_grammar_revision_internal(v);
-    assert_eq!(result, 0);
-}
-/*
-#[test]
-fn bad() {
-    let statement_str = "/foo/";
+fn assert_view_history() {
+    let statement_str = "@view #1";
     let raw = get_parse_raw(&statement_str);
     assert_eq!(raw.result.is_empty() && !raw.error.is_empty(), false);
 }
- */
+#[test]
+fn assert_view_macros() {
+    let statement_str = "@view from 1/1/2024 to 4/4/2924";
+    let raw = get_parse_raw(&statement_str);
+    assert_eq!(raw.result.is_empty() && !raw.error.is_empty(), false);
+}
