33e9359e41e3a77058934c43e606950d8e02cb4b
Kevin Wonus <kevin@wonus.com>
2025-02-15 23:21:40 -0500

    Adding long lost delta

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+cargo build --target=x86_64-pc-windows-msvc --all-targets
+cargo build --target=x86_64-pc-windows-msvc --release
+
 pushd ../AV-Console/bin/Debug/net8.0 > /dev/null
 rm -f pinshot_blue.*
 popd > /dev/null
@@ -10,24 +13,25 @@ popd > /dev/null
 pushd ../AV-API/bin/Release/net8.0
 rm -f pinshot_blue.*
 popd > /dev/null
-pushd ../AV-Bible/bin/Debug/net8.0-windows10.0.17763.0 > /dev/null
+pushd ../AV-Bible/bin/x64/Debug/net8.0-windows10.0.17763.0 > /dev/null
 rm -f pinshot_blue.*
 popd > /dev/null
 pushd ../AV-Bible/bin/x64/Release/net8.0-windows10.0.17763.0 > /dev/null
 rm -f pinshot_blue.*
 popd > /dev/null
 echo copy debug binaries to AV-Console
-cp target/debug/*.dll ../AV-Console/bin/Debug/net8.0
-cp target/debug/*.pdb ../AV-Console/bin/Debug/net8.0
+cp target/x86_64-pc-windows-msvc/debug/*.dll ../AV-Console/bin/Debug/net8.0
+cp target/x86_64-pc-windows-msvc/debug/*.pdb ../AV-Console/bin/Debug/net8.0
 
-echo copy debug binaries to AV-Bible
-cp target/debug/*.dll ../AV-Bible/bin/Debug/net8.0-windows10.0.17763.0
-cp target/debug/*.pdb ../AV-Bible/bin/Debug/net8.0-windows10.0.17763.0
+echo copy debug binaries to AV-Bible and AV-API and AV-Data-Manager
+cp target/x86_64-pc-windows-msvc/debug/*.dll ../AV-Bible/bin/x64/Debug/net8.0-windows10.0.17763.0
+cp target/x86_64-pc-windows-msvc/debug/*.pdb ../AV-Bible/bin/x64/Debug/net8.0-windows10.0.17763.0
+cp target/x86_64-pc-windows-msvc/debug/*.dll ../AV-API/bin/Debug/net8.0
+cp target/x86_64-pc-windows-msvc/debug/*.pdb ../AV-API/bin/Debug/net8.0
+cp target/x86_64-pc-windows-msvc/debug/*.dll ../AV-Data-Manager/bin/Debug/net8.0-windows7.0
+cp target/x86_64-pc-windows-msvc/debug/*.pdb ../AV-Data-Manager/bin/Debug/net8.0-windows7.0
 
-echo copy debug binaries to AV-API
-cp target/debug/*.dll ../AV-API/bin/Debug/net8.0
-cp target/debug/*.pdb ../AV-API/bin/Debug/net8.0
-
-echo copy release binaries to AV-Bible and AV-API
-cp target/release/*.dll ../AV-Bible/bin/x64/Release/net8.0-windows10.0.17763.0
-cp target/release/*.dll ../AV-API/bin/Release/net8.0
+echo copy release binaries to AV-Bible and AV-API and AV-Data-Manager
+cp target/x86_64-pc-windows-msvc/release/*.dll ../AV-Bible/bin/x64/Release/net8.0-windows10.0.17763.0
+cp target/x86_64-pc-windows-msvc/release/*.dll ../AV-API/bin/Release/net8.0
+cp target/x86_64-pc-windows-msvc/release/*.dll ../AV-Data-Manager/bin/Release/net8.0-windows7.0
